8304cfdf053152b84e479b9ef4903c8cb6972074
FrancisFlow <iamngigikaranja1@aol.com>
2021-12-09 08:45:39 +0300

    Created Test class and tested for proper initialization

---
+++ networks_test.py
@@ -0,0 +1,44 @@
+import unittest
+from networks import Credentials
+
+class TestCredentials(unittest.TestCase):
+
+    """
+    test class that defines test cases for credentials classes
+
+    Args:
+    unittest.TestCase: helps in creating test cases
+
+    """
+
+    def setUp(self):
+        """
+        setuP method that runs before each test case
+        """
+        self.new_network = Credentials("Lichess", "FrancisFlow", "Happy@allT")
+
+    def tearDown(self):
+        """
+        method to clear fields after unit tests are run
+        """
+        self.network_list=[]
+
+    
+    def test_init(self):
+
+
+        """
+        test whether the objects are initialized correctly
+
+        """
+        self.assertEqual(self.new_network.network_name, "Lichess")
+        self.assertEqual(self.new_network.network_username, "FrancisFlow")
+        self.assertEqual(self.new_network.network_password, "Happy@allT")
+
+
+
+
+
+
+if __name__=='__main__':
+    unittest.main()
